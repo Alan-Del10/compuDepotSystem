124f18a3d8ce4d52c2669c3a2268e86e147d5a85
Alan Delgado <alaneduarddc10@outlook.com>
2021-04-21 15:52:07 -0500

    Update InventarioController.php

--- app/Http/Controllers/InventarioController.php
+++ app/Http/Controllers/InventarioController.php
@@ -721,8 +721,8 @@ class InventarioController extends Controller
         ->send();*/
 
         //Etiquetas para cada stock de producto
-        if (Storage::disk('local')->exists('inventario/etiqueta/' . $inventario[0]->upc . '-2.pdf')) {
-            Storage::disk('local')->delete('inventario/etiqueta/' . $inventario[0]->upc . '-2.pdf');
+        if (Storage::disk('public')->exists('inventario/etiqueta/' . $inventario[0]->upc . '-2.pdf')) {
+            Storage::disk('public')->delete('inventario/etiqueta/' . $inventario[0]->upc . '-2.pdf');
         }
         try {
             $imagen = base64_encode(public_path("storage/sucursales/" . $sucursal[0]->logo));
@@ -746,17 +746,15 @@ class InventarioController extends Controller
 
         try {
             $documento = PDF::loadView('Inventario.etiquetav2', $datos)->setPaper('b8', 'landscape')->setWarnings(false)->output();
-                //->move(public_path("storage/inventario"), $inventario[0]->upc . '-2.pdf');
-            Storage::disk('local')->put('inventario/etiqueta'.'/'.$inventario[0]->upc . '-2.pdf', $documento);
-            //Storage::
-            //$documento->save(public_path("storage/inventario").$inventario[0]->upc . '-2.pdf');
+            Storage::disk('public')->put('inventario/etiqueta'.'/'.$inventario[0]->upc . '-2.pdf', $documento);
         } catch (\Throwable $th) {
+            dd('mal');
             return 2;
         }
         try {
             Printing::newPrintTask()
                 ->printer($sucursal[0]->etiquetas)
-                ->file(storage_path('app\\inventario\\etiqueta\\') . $inventario[0]->upc . '-2.pdf')
+                ->file(public_path("storage/inventario/etiqueta/" . $inventario[0]->upc . '-2.pdf'))
                 ->send();
         } catch (\Throwable $th) {
             return 3;
